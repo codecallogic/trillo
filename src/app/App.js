6eed8e57a26485effee5044396010bb7b121b4d4
Fabricio <jfguardiacpw@gmail.com>
2020-07-22 23:02:38 -0700

    set up overall layout and flex containers

--- src/app/App.js
+++ src/app/App.js
@@ -1,11 +1,21 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Route, Switch} from 'react-router-dom'
+import Header from '../components/header'
+import Content from '../components/content'
 
 function App() {
   return (
-    <div>
-      Hello
+    <div className="container">
+      <Switch>
+        <Route exact path={"/"} render={() => 
+          <div>
+          <Header />
+          <Content />
+          </div>
+        }/>
+      </Switch>
     </div>
   );
 }
---
+++ src/components/content.jsx
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+class Content extends Component {
+    render () {
+        return (
+            <div className="content">
+                <nav className="sidebar">
+                    Navigation
+                </nav>
+                <main className="hotel-view">
+                    Hotel View
+                </main>
+            </div>
+        )
+    }
+}
+export default Content
