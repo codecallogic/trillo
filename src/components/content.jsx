54df61307a3cad7eb03e36a8733ce8586c337c30
Fabricio <jfguardiacpw@gmail.com>
2020-07-24 12:49:49 -0700

    building the sidebar

--- src/components/content.jsx
+++ src/components/content.jsx
@@ -5,7 +5,44 @@ class Content extends Component {
         return (
             <div className="content">
                 <nav className="sidebar">
-                    Navigation
+                    <ul className="side-nav">
+                        <li className="side-nav_item">
+                            <a href="#" className="side-nav_link">
+                                <svg className="side-nav_icon">
+                                    <use xlinkHref="/images/sprite.svg#icon-home3"></use>
+                                </svg>
+                                <span>Hotel</span>
+                            </a>
+                        </li>
+                        <li className="side-nav_item">
+                            <a href="#" className="side-nav_link">
+                                <svg className="side-nav_icon">
+                                    <use xlinkHref="/images/sprite.svg#icon-aircraft"></use>
+                                </svg>
+                                <span>Flight</span>
+                            </a>
+                        </li>
+                        <li className="side-nav_item">
+                            <a href="#" className="side-nav_link">
+                                <svg className="side-nav_icon">
+                                    <use xlinkHref="/images/sprite.svg#icon-key"></use>
+                                </svg>
+                                <span>Car Rental</span>
+                            </a>
+                        </li>
+                        <li className="side-nav_item">
+                            <a href="#" className="side-nav_link">
+                                <svg className="side-nav_icon">
+                                    <use xlinkHref="/images/sprite.svg#icon-map"></use>
+                                </svg>
+                                <span>Tours</span>
+                            </a>
+                        </li>
+                    </ul>
+
+                    <div className="legal">
+                        &copy; 2020 by Trillo. All Rights Reserved.
+                    </div>
                 </nav>
                 <main className="hotel-view">
                     Hotel View
